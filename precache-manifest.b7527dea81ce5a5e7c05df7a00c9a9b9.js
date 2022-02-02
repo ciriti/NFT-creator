d420c8578a9bc1f75fc770aac762dadff8cc0972
ciriti <ciriti@gmail.com>
2022-02-02 12:40:18 +0100

    Updates

---
+++ precache-manifest.b7527dea81ce5a5e7c05df7a00c9a9b9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ada4682a41c0e7afffc41282683cde9b",
+    "url": "/NFT-creator/index.html"
+  },
+  {
+    "revision": "0ca06b06c082721ccd2f",
+    "url": "/NFT-creator/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "ae00c46927c04f91ef0d",
+    "url": "/NFT-creator/static/js/2.182024ba.chunk.js"
+  },
+  {
+    "revision": "0ca06b06c082721ccd2f",
+    "url": "/NFT-creator/static/js/main.64d43fd5.chunk.js"
+  },
+  {
+    "revision": "adca0941675b9c6cab3e",
+    "url": "/NFT-creator/static/js/runtime-main.f50d23cd.js"
+  }
+]);
